7f999b41a5673e08b3fc0631d3533279c4f9d6de
Florian <florian.dauv@gmail.com>
2024-04-21 11:55:20 +0200

    refactor: création d'un repository mieux structuré basé sur l'ancien repo 'evaluateur_de_propositions '

---
+++ AnalyseurSyntaxique.py
@@ -0,0 +1,102 @@
+from Grammaire import lexer, parser
+from terminaltables import AsciiTable
+
+def afficher_aide():
+    """
+    Affiche un message d'aide avec la liste des symboles valides pour les formules de logique de proposition.
+    """
+
+    # Définition du tableau des correspondances connecteur / symbole
+    tableau_de_correspondace = AsciiTable([
+        ["Connecteur", "Symbole"],
+        # Connecteur correspondant au symbole de négation
+        ["Négation (¬)",    "~"],
+        # Connecteur correspondant au symbole de conjonction
+        ["Conjonction (∧)", "&"],
+        # Connecteur correspondant au symbole de disjonction
+        ["Disjonction (∨)", "|"],
+        # Connecteur correspondant au symbole d'implication
+        ["Implication (→)", ">>"],
+        # Connecteur correspondant au symbole d'équivalence
+        ["Équivalence (↔)", "="]
+    ]).table
+
+    # Affichage de la table de correspondance
+    print("Tableau de correspondance")
+    print(tableau_de_correspondace)
+
+    # Affichage du message d'aide
+    print("\n-----Taper votre formule-----")
+    print("Exemple : (P | Q) >> (~r = (Q & P))\n")
+
+
+def convertir_mots_en_symboles(expression):
+    """
+    Convertit les mots-clés en symboles logiques.
+    """
+
+    mots_en_symboles = {
+        "~": "¬",
+        "&": "∧",
+        "|": "∨",
+        ">>": "→",
+        "=": "↔"
+    }
+
+    expression_convertie = expression
+
+    for mots, symboles in mots_en_symboles.items():
+        expression_convertie = expression_convertie.replace(mots, symboles)
+
+    return expression_convertie
+
+
+def convertir_saisie():
+    """
+    Demande a l'utilisateur de rentrer une formule et la convertit.
+    """
+
+    # Saisie de la formule
+    saisie = input('Votre formule : ')
+
+    # Si la saisie est vide, retour à la saisie
+    if not saisie:
+        return False
+
+    # Afficher l'aide si l'utilisateur tape "help"
+    if saisie == 'help':
+        afficher_aide()
+
+    # Convertir la syntaxe de la formule saisie
+    return convertir_mots_en_symboles(saisie)
+
+
+def analyser_formule(formule):
+    """
+    Analyse la formule et vérifie si elle est correcte.
+    """
+
+    # Executer le parseur
+    try:
+        # Si le parseur renvoie un résultat la formule est correcte
+        if (parser.parse(formule, lexer=lexer)):
+            return True
+    except:
+        # Si le parseur ne renvoie pas de résultat la formule est incorrecte
+        return False
+
+
+if __name__ == "__main__":
+
+    # Afficher l'aide au démarrage
+    afficher_aide()
+
+    while True:
+        # Demander a l'utilisateur de rentrer une formule
+        formule = convertir_saisie()
+
+        # Afficher si la formule est correcte
+        if analyser_formule(formule):
+            print("Formule correcte : {}".format(formule))
+        else:
+            print("Formule incorrecte : {}".format(formule))
